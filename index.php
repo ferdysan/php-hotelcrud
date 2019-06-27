bd8f155c5a8e82982ad5df3b7dae722dba6bfa66
Ferdysan <ferdysan87@gmail.com>
2019-06-27 09:18:17 +0200

    connessione db effettuata

---
+++ index.php
@@ -0,0 +1,68 @@
+<!-- effettuo la connessione al database -->
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "db_hotel";
+
+$connessione = new mysqli($servername, $username, $password, $dbname );
+
+if($connessione && $connessione->connect_error){
+  echo ("Connection failed: " . $connessione->connect_error);
+  exit();
+}
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <table>
+    <tr>
+      <th>stanza N°</th>
+      <th>piano</th>
+      <th>Letti</th>
+    </tr>
+
+
+    <?php
+    $sql = "SELECT *, floor FROM stanze ";
+    // funzione query la freccia e come la notazione punto
+    $result = $connessione->query($sql);
+
+    // controllo quante righe ci sono nel risultato della mia chiamata al database
+    if ($result && $result->num_rows > 0) {
+    // output data of each row
+    // prendi una riga dei tuoi risultati e salvalo nella variabile row una riga per volta
+    while($row = $result->fetch_assoc()) {?>
+
+        <tr>
+            <td><?php echo $row['room_number'] ?></td>
+            <td><?php echo $row['floor'] ?></td>
+            <td><?php echo $row['beds'] ?></td>
+        </tr>
+      </table>
+
+      <?php
+       }
+    } elseif ($result) {
+       echo "0 results";
+    } else {
+      echo "query error";
+    }
+    $connessione->close();
+
+     ?>
+
+
+
+
+  </body>
+</html>
